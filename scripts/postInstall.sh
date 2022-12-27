74f799473ffaecbe37e828d28045fbfa5d2efb84
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-27 16:35:54 +0200

    fix

---
+++ scripts/postInstall.sh
@@ -0,0 +1,10 @@
+
+
+target=$(docker-compose port app 8080)
+
+curl http://${target}/api/admin/config/key \
+  -H 'content-type: text/plain;charset=UTF-8' \
+  -u "admin:abc123" \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+  --data-raw '{"value":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
