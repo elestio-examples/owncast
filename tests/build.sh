48b6a68ea4458cd757a01b2e82359f8509abed2d
rpeshnegi <67201900+rpeshnegi@users.noreply.github.com>
2023-05-24 11:21:06 +0530

    Added test

---
+++ tests/build.sh
@@ -0,0 +1,2 @@
+docker build . --tag elestio4test/owncast:latest;
+TAG_TO_CHANGE
---
+++ tests/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p ./data
+mkdir -p ./data/emoji
+mkdir -p ./data/tmp
+
+chown -R 1000:1000 ./data
+chown -R 1000:1000 ./data/emoji
+chown -R 1000:1000 ./data/tmp
+
+chmod -R 777 ./data
+
+docker-compose up -d;
+sleep 30s;
