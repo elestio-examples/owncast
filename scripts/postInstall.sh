f123a8d4d6c8432fbd719b200db167b7068c089a
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-12 17:35:48 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -6,7 +6,7 @@ sleep 10s;
 
 target=$(docker-compose port app 8080)
 
-curl http://${target}/api/admin/config/key \
+curl http://${target}/api/admin/config/adminpass \
   -H 'content-type: text/plain;charset=UTF-8' \
   -u "admin:abc123" \
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
